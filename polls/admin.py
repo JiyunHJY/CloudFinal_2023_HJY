5a7c6d96863ce7406373f4f5901f41909ad07eff
JiyunHJY <hwangjiyun7377@gmail.com>
2023-06-19 18:58:40 +0900

    질문가능개수 추가

--- polls/admin.py
+++ polls/admin.py
@@ -2,9 +2,6 @@ from django.contrib import admin
 # Register your models here.
 from .models import Question, Choice
 
-# admin.site.register(Question)
-# admin.site.register(Choice)
-
 admin.site.site_header = "Pollster Admin"
 admin.site.site_title = "Pollster Admin Area"
 admin.site.index_title = "Welcome to the Pollster Admin Area"
@@ -12,7 +9,7 @@ admin.site.index_title = "Welcome to the Pollster Admin Area"
 
 class ChoiceInLine(admin.TabularInline):
     model = Choice
-    extra = 3
+    extra = 5
 
 
 class QuestionAdmin(admin.ModelAdmin):
--- pollster/urls.py
+++ pollster/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for survey project.
+URL configuration for pollster project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
